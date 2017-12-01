4be5cf5752da251bd5fa8850687223fe0c41e9d0
KarneyenkaDzmitry <33957610+KarneyenkaDzmitry@users.noreply.github.com>
2017-12-01 14:49:48 +0300

    My experience/
    Signed-off-by: KarneyenkaDzmitry <33957610+KarneyenkaDzmitry@users.noreply.github.com>

--- 4.JavaCollections/src/com/javarush/task/task31/task3112/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3112/Solution.java
@@ -7,6 +7,7 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 /* 
 Загрузчик файлов
@@ -14,7 +15,7 @@ import java.nio.file.Paths;
 public class Solution {
 
     public static void main(String[] args) throws IOException {
-        Path passwords = downloadFile("https://yastatic.net/morda-logo/i/citylogos/yandex19-logo-ru.png", Paths.get("D:/MyDownloads"));
+        Path passwords = downloadFile("https://yastatic.net/morda-logo/i/citylogos/yandex19-logo-ru.png", Paths.get("D:\\Java\\Work\\Task3101"));
         for (String line : Files.readAllLines(passwords)) {
             System.out.println(line);
         }
@@ -24,11 +25,17 @@ public class Solution {
         URL url = new URL(urlString);
         Path tempFile = Files.createTempFile("temp-", ".tmp");
         InputStream input  =url.openStream();
-        Files.copy(input, tempFile);
+        //Path tempFile = Files.createFile(Paths.get("D:\\Java\\Work\\Task3101\\new7.tmp"));
+        Files.copy(input, tempFile, StandardCopyOption.REPLACE_EXISTING);
         String nameFiles[] = urlString.split("\\/");
         String name = nameFiles[nameFiles.length-1];
-        Path path = Paths.get(downloadDirectory.toAbsolutePath().toString()+"/"+name);
-        Files.move(tempFile, path);
+        Path path = Paths.get(downloadDirectory.toAbsolutePath().toString(), name);
+        System.out.println(path);
+        Files.move(tempFile, path/*, StandardCopyOption.REPLACE_EXISTING*/);
+
+        /* String fieName=urlString.substring(urlString.lastIndexOf("/"));
+        Path destPath=Paths.get(downloadDirectory.toString(), fieName);
+Files.move(tmp,destPath);*/
        return path; // implement this method
     }
 }
